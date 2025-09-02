8fcab87b792dddd1e3d991ae4aac6cba883baad3
Felipe Ferrarini <ferrarinifelipe@gmail.com>
2025-09-02 19:45:04 -0300

    Add main entry point to export button and spinner components

--- src/components/ui/button/button.spec.tsx
+++ src/components/ui/button/button.spec.tsx
@@ -2,7 +2,7 @@ import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 import { afterEach, describe, expect, it, vi } from 'vitest';
 import { Button } from './button.component';
 
-describe('MyButton test:', () => {
+describe('Button test:', () => {
   afterEach(cleanup);
 
   it('should render component', () => {
